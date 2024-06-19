2d439dba96771117bf8168895b2224b6a8a1a522
aminos2020 <amin.os2012@gmail.com>
2024-06-19 15:02:25 +0330

    final 1

--- app/api/get-data/route.ts
+++ app/api/get-data/route.ts
@@ -1,14 +1,31 @@
 // pages/api/get-data.ts
 import { NextRequest, NextResponse } from "next/server";
-import fs from "fs";
+import { promises as fs } from "fs";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
   try {
-    // Read data from the JSON file
-    const dataBuffer = fs.readFileSync("data.json");
+    const dataBuffer = await fs.readFile("data.json");
     const data = JSON.parse(dataBuffer.toString());
     console.log("data is", data);
-    return NextResponse.json(data);
+    const response = NextResponse.json(data);
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const data = await req.json();
+    console.log("Received data:", data);
+    await fs.writeFile("data.json", JSON.stringify(data, null, 2));
+    const response = NextResponse.json({
+      message: "Data received and saved successfully",
+    });
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    return response;
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error: "An error occurred" }, { status: 500 });
