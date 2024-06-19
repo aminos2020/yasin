63071bc31fe171ce80af4f7fb563df674ac61257
aminos2020 <amin.os2012@gmail.com>
2024-06-19 15:11:10 +0330

    final 3

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Users() {
   const [responseData, setResponseData] = useState(null);
